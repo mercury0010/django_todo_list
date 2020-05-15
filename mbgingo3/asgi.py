01d4ee3b84d8fc0b42f1f421d6605d37c8d6424b
gingomercury@gmail.com <gingomercury@gmail.com>
2020-05-15 14:30:23 +0800

    Initial Commit

---
+++ mbgingo3/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mbgingo3 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mbgingo3.settings')
+
+application = get_asgi_application()
